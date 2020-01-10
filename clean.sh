c896054b3e1a4f42d440f07d26ec50ee80b0cf6b
Hany Abdulsamad <abdulsamad@ias.informatik.tu-darmstadt.de>
2020-01-10 11:00:45 +0100

    add clean script

---
+++ clean.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+arg=${1:-.}
+exts="aux bbl blg brf idx ilg ind lof log lol\
+  lot out toc xmpi run.xml xmpdata bcf synctex.gz\
+  fdb_latexmk fls acn syo xdy opo mtc* maf"
+
+if [ -d $arg ]; then
+    for ext in $exts; do
+         rm -f $arg/*.$ext
+    done
+else
+    for ext in $exts; do
+         rm -f $arg.$ext
+    done
+fi
